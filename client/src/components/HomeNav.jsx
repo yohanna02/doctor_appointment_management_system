163b985fbcae43ba3b0a194f69bfcad040ee4b23
Raymond A.A Amem <raymondamem525@gmail.com>
2022-06-28 12:35:38 +0100

    added logo to nav ui

--- client/src/components/HomeNav.jsx
+++ client/src/components/HomeNav.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import logo from "../img/logo.jpg";
 export default function Nav() {
     return (
         <header className="header">
             <input type="checkbox" id="checkbox1"/>
             <nav className="container">
                 <label htmlFor="checkbox1" className="checkbox1"></label>
-                <Link role="logo" to="/" title="ODAMS" aria-label="Online Doctor Appointment Management System Web Logo">ODA<span>MS</span></Link>
+                <Link role="logo" to="/" title="ODAMS Arthur Javis university Logo" aria-label="Online Doctor Appointment Management System Web Logo" style={{padding: 0}}><img src={logo} alt="Arthur Javis university" width={100} height={50}/></Link>
                 <Link to="/doctordashboard/appointment" title="ODAMS link" aria-label="Online Doctor Appointment Management System link">Online Doctor Appointment Management System</Link>
                 <Link to="/login" title="ODAMS login" aria-label="Online Doctor Appointment Management System Web Login">Login</Link>
             </nav>
