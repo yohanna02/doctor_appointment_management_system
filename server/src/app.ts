7b9cd9b08c85ed274ea1e13fb89e5db875d2fb10
Yohanna Philip  Abana <57155932+yohanna02@users.noreply.github.com>
2022-11-18 11:38:51 +0000

    switch d data base moongo atlas and updated package.json file scripts

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,14 +13,14 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/docter_appointment_management_system")
+mongoose.connect("mongodb+srv://yohanna02:bbPSTFu3NkHTfoB6fgK8@cluster0.joi010b.mongodb.net/?retryWrites=true&w=majority")
 .then(() => {
     console.log("Connected to mongodb server successfully");
 }).catch((err) => {
     throw err;
 });
 
-export const agenda = new Agenda({ db: { address: "mongodb://localhost:27017/docter_appointment_management_system" } });
+export const agenda = new Agenda({ db: { address: "mongodb+srv://yohanna02:bbPSTFu3NkHTfoB6fgK8@cluster0.joi010b.mongodb.net/?retryWrites=true&w=majority" } });
 (async function () {
     initAppointmentJob(agenda);
     await agenda.start();
